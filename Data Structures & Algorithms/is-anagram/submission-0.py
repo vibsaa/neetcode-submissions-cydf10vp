88d5e2546146caadcb690c650fe71a7dc107e208
VIBHANSHU SHARMA <vibhanshu.ev19@nsut.ac.in>
2026-04-06 23:35:22 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # #number of char and char type shud match
+        # s_count ={}
+        # t_count ={}
+        # if len(s)!=len(t):
+        #     return False
+        
+        # for i in range(len(s)):
+        #     s_count[s[i]] = 1 + s_count.get(s[i],0) 
+        #     t_count[t[i]] = 1 + t_count.get(t[i],0) 
+
+        # for c in s_count:
+        #     if s_count[c] != t_count.get(c,0):
+        #         return False
+        # return True
+
+
+        #or
+        return Counter(s) == Counter(t)
+
+
+
+            
+
+
+                
+
+        
